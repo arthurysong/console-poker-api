af74bf41367f7086f3accb7a5d81354efc199654
Arthur Song <arth3rs0ng@gmail.com>
2020-07-06 18:19:34 -0700

    Changing JWT to use ENV for secret key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,13 +3,20 @@ class JsonWebToken
         def encode(payload)
         # def encode(payload, exp = 24.hours.from_now)
             # payload[:exp] = exp.to_i
-            puts Rails.application.secrets.secret_key_base
-            JWT.encode(payload, Rails.application.secrets.secret_key_base)
+            # puts Rails.application.secrets.secret_key_base
+            puts ENV['SECRET_KEY_BASE']
+            # JWT.encode(payload, Rails.application.secrets.secret_key_base)
+            # dev env
+            JWT.encode(payload, ENV['SECRET_KEY_BASE'])
+
         end
 
         def decode(token)
             # JWT.decode(request['token'], Rails.application.secrets.secret_key_base)
-            body = JWT.decode(token, Rails.application.secrets.secret_key_base)
+
+            # body = JWT.decode(token, Rails.application.secrets.secret_key_base)
+            #dev env
+            body = JWT.decode(token, ENV['SECRET_KEY_BASE'])
             HashWithIndifferentAccess.new body 
         rescue
             nil
