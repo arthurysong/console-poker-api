6031ec1bc1ad78582021cfdf68faa69283d663a4
Arthur Song <arth3rs0ng@gmail.com>
2020-07-07 16:03:20 -0700

    Updated Secret key in ActionCable Channel

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,7 +1,8 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
     def find_verified_user 
-      if current_user = User.find(JWT.decode(params['token'], Rails.application.secrets.secret_key_base)[0]["user_id"])
+      # if current_user = User.find(JWT.decode(params['token'], Rails.application.secrets.secret_key_base)[0]["user_id"])
+      if current_user = User.find(JWT.decode(params['token'], ENV['SECRET_KEY_BASE'])[0]["user_id"])
         current_user
       else
         reject
