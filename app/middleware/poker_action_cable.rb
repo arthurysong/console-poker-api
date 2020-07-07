72c77cad06a4518dba0aa666e78f029b9b6bb6d5
Arthur Song <arth3rs0ng@gmail.com>
2020-07-06 18:49:27 -0700

    Fixing bug in PokerActionCable middleware

--- app/middleware/poker_action_cable.rb
+++ app/middleware/poker_action_cable.rb
@@ -1,10 +1,10 @@
-class PokerActionCable
+class PokerActionCable < ActionCable::Connection::WebSocket
     def initialize(app, options={})
       @app = app
     end
   
     def call(env)
-      if Faye::WebSocket.websocket?(env)
+      if ::WebSocket::Driver.websocket?(env)
         ActionCable.server.call(env)
       else
         @app.call(env)
