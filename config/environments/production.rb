03e48c55206b298ea131a6cc68fca7c5e9a13d1d
Arthur Song <arth3rs0ng@gmail.com>
2020-07-06 18:45:29 -0700

    Changing configuration so we can use ActionCable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.middleware.use PokerActionCable 
+  config.web_socket_server_url = "wss://console-poker-api.herokuapp.com/"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
