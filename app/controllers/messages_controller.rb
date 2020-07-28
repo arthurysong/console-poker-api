79179ff28cb0889be1e1f6e2b3358df23a554388
Arthur Song <arth3rs0ng@gmail.com>
2020-07-28 14:44:50 -0700

    Messages#create renders json so that client can log without erroring

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,20 @@
+require 'pry'
+
 class MessagesController < ApplicationController
     def create 
         user = @current_user
         room = user.room
-        m = Message.create(payload: message_params["content"], user: user, chatbox: room.chatbox)
+        # binding.pry
+        m = Message.create(payload: message_params["message"], user: user, chatbox: room.chatbox)
     
         ActionCable.server.broadcast("room_#{room.id}", {type: "new_message", message: m })
+
+        render json: { success: "Message sent" }, status: 201
     end
 
     private 
 
     def message_params
-        params.permit(:content)
+        params.permit(:message)
     end
 end
