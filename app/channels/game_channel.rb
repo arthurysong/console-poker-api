6e3f92955d31d6ec46fbe81436506b05149134fe
Arthur Song <arthursong14@gmail.com>
2020-08-17 14:37:29 -0700

    For game#unsubscribed, user_leave only gets broadcast if user was sitting at the table

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -9,14 +9,15 @@ class GameChannel < ApplicationCable::Channel
     user = find_verified_user
     game = Game.find(params["game"])
 
-    user.round.player_has_left(user) if user.round && user.round.is_playing #if user is in a round
-    
-    i = game.unsit(user)
-    user.game_id = nil
-    user.reset_user
-    user.save
+    if user.game_id == game.id 
+      user.round.player_has_left(user) if user.round && user.round.is_playing #if user is in a round
+      i = game.unsit(user)
+      user.game_id = nil
+      user.reset_user
+      user.save
+      ActionCable.server.broadcast("game_#{game.id}", { type: "user_leave", startable: game.startable, seat_index: i })
+    end
 
-    ActionCable.server.broadcast("game_#{game.id}", { type: "user_leave", startable: game.startable, seat_index: i })
     stop_all_streams
   end
 end
